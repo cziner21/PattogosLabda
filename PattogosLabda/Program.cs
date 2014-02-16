7bbadfa834759c5bfb4444f30e0bb02606780155
cziner21 <cziner.adam@gmail.com>
2014-02-16 12:29:20 +0100

    Alap pattogós labda, szál nélküli
    Pattogós labda program megírása. Készítsünk egy labda osztályt, amely konstruktora random helyre lerak egy o betűt a képernyőre és beállít egy random irányt ennek a "labdának". Ezután írjuk meg a mozog metódust, amely vezérli a labda mozgását és az ablak széleinél az irányváltást (végtelen ideig). A képernyő 25 soros (0..24), és 80 oszlopos (0..79).

---
+++ PattogosLabda/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PattogosLabda
+{
+    class PattogosLabda
+    {
+        int helyX;
+        int helyY;
+        int iranyX;
+        int iranyY;
+        Random rnd = new Random();
+
+        public PattogosLabda() {
+            this.helyX = rnd.Next(0, 80);
+            this.helyY = rnd.Next(0, 25);
+            this.iranyX = rnd.Next(0, 80);
+            this.iranyY = rnd.Next(0, 25);
+            Console.SetCursorPosition(helyX, helyY);
+            Console.Write("O");
+
+        }
+
+        public void Mozog() {
+            while (true) {
+                if (iranyX < helyX && iranyY < helyY) {
+                    Console.Write(" ");
+                    Console.SetCursorPosition(helyX - 1, helyY - 1);
+                    Console.Write("O");
+                }
+                else if (iranyX < helyX && iranyY > helyY) {
+                    Console.Write(" ");
+                    Console.SetCursorPosition(helyX - 1, helyY + 1);
+                    Console.Write("O");
+                }
+                else if (iranyX > helyX && iranyY < helyY) {
+                    Console.Write(" ");
+                    Console.SetCursorPosition(helyX + 1, helyY - 1);
+                    Console.Write("O");
+                }
+                else if (iranyX > helyX && iranyY > helyY) {
+                    Console.Write(" ");
+                    Console.SetCursorPosition(helyX + 1, helyY + 1);
+                    Console.Write("O");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+
+            PattogosLabda labda = new PattogosLabda();
+            labda.Mozog();
+            Console.ReadKey();
+        }
+    }
+}
